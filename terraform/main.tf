ac7e6abc27ad0bedc22a9b80e6a1132da997b74b
Narava <raviprasad.narava@accenture.com>
2026-09-14 21:56:12 +1000

    Adding User  details for provider Authentication

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,7 @@ terraform {
 provider "snowflake" {
     account_name = "AL32991"
     organization_name = "IVULRLR"
+    user = "GITHUB_DCM_SVC"
 }
 resource "snowflake_account_role" "tf_platform_role" {
     name = "TF_PLATFORM_ROLE"
