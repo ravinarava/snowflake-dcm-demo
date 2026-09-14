ea6d2d704aa8b7cbd16707c0f63e2b6dfc398006
Narava <raviprasad.narava@accenture.com>
2026-09-14 21:53:25 +1000

    Adding Account details for provider Authentication

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,10 @@ terraform {
         }
       }
 }
+provider "snowflake" {
+    account_name = "AL32991"
+    organization_name = "IVULRLR"
+}
 resource "snowflake_account_role" "tf_platform_role" {
     name = "TF_PLATFORM_ROLE"
 }
