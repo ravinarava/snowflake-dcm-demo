20c5d0d6dea0308c103099b5ae26e9e7994028c3
Narava <raviprasad.narava@accenture.com>
2026-09-17 23:20:04 +1000

    Added providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,12 +5,6 @@ terraform {
         }
       }
 }
-provider "snowflake" {
-    authenticator = "WORKLOAD_IDENTITY"
-    account_name = "AL32991"
-    organization_name = "IVULRLR"
-    user = "GITHUB_DCM_SVC"
-}
 resource "snowflake_account_role" "tf_platform_role" {
     name = "TF_PLATFORM_ROLE_V1"
 }
