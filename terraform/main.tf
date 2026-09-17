804d1ac45daa0912363b90f2e3930926243d0302
Narava <raviprasad.narava@accenture.com>
2026-09-17 23:24:17 +1000

    Adding versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,3 @@
-terraform {
-      required_providers {
-        snowflake = {
-            source = "snowflakedb/snowflake"
-        }
-      }
-}
 resource "snowflake_account_role" "tf_platform_role" {
     name = "TF_PLATFORM_ROLE_V1"
 }
