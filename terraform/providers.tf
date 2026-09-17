0bbcece8cf652765df29bb8ec0d74ed1a6bfebab
Narava <raviprasad.narava@accenture.com>
2026-09-18 09:42:22 +1000

    Added test yml

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,7 +2,7 @@ provider "snowflake" {
     authenticator = "WORKLOAD_IDENTITY"
     account_name = "AL32991"
     organization_name = "IVULRLR"
-    user = "GITHUB_DCM_SVC"
+  
 
     role = "ACCOUNTADMIN"
 }
